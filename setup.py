0f70a6d6900ffe5c254862f3a94badf302af8fdd
briandesilva <bdesilva@uw.edu>
2020-06-29 12:42:56 -0700

    Add initial top-level files

---
+++ setup.py
@@ -0,0 +1,52 @@
+import pathlib
+import sys
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+assert sys.version_info >= (3, 6, 0), "pySensors requires Python 3.6+"
+
+NAME = "pysensors"
+DESCRIPTION = "Sparse sensor placement"
+URL = "https://github.com/dynamicslab/pysensors"
+EMAIL = "emily.e.clark93@gmail.com, kmanohar@uw.edu, bdesilva@uw.edu"
+AUTHOR = "Emily Clark, Krithika Manohar, Brian de Silva"
+PYTHON = ">=3.6"
+LICENSE = "MIT"
+CLASSIFIERS = [
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Science/Research",
+    "License :: OSI Approved :: MIT License",
+    "Topic :: Scientific/Engineering :: Mathematics",
+]
+
+here = pathlib.Path(__file__).parent
+
+with open(here / "requirements.txt", "r") as f:
+    REQUIRED = f.readlines()
+
+with open(here / "README.rst", "r") as f:
+    LONG_DESCRIPTION = f.read()
+
+
+setup(
+    name=NAME,
+    use_scm_version=True,
+    setup_requires=["setuptools_scm", "setuptools_scm_git_archive"],
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/x-rst",
+    author=AUTHOR,
+    author_email=EMAIL,
+    url=URL,
+    packages=find_packages(exclude=["test", "examples"]),
+    install_requires=REQUIRED,
+    python_requires=PYTHON,
+    license=LICENSE,
+    classifiers=CLASSIFIERS,
+)

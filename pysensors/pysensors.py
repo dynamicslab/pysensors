d95902fba5aeb17a67f5b42a6c156a4b5ce60784
briandesilva <bdesilva@uw.edu>
2020-10-05 16:01:29 -0700

    Fix docstring

--- pysensors/pysensors.py
+++ pysensors/pysensors.py
@@ -148,7 +148,7 @@ class SensorSelector(BaseEstimator):
         x: array-like, shape (n_samples, n_sensors)
             Measurements from which to form prediction.
             The measurements should be taken at the sensor locations specified by
-            ``self.get_ranked_sensors()``.
+            ``self.get_selected_sensors()``.
 
         solve_kws: dict, optional
             keyword arguments to be passed to the linear solver used to invert

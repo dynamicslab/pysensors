1bc277859337ec33885eb7fe225f63546b82323e
briandesilva <bdesilva@uw.edu>
2020-10-05 17:53:41 -0700

    Test for CCQR equivalence to QR if no costs

--- test/optimizers/test_optimizers.py
+++ test/optimizers/test_optimizers.py
@@ -1,4 +1,7 @@
 """Unit tests for optimizers"""
+import numpy as np
+
+from pysensors.optimizers import CCQR
 from pysensors.optimizers import QR
 
 
@@ -9,3 +12,14 @@ def test_num_sensors(data_vandermonde):
     qr = QR()
     sensors = qr.get_sensors(x.T)
     assert len(sensors) == max_sensors
+
+
+def test_ccqr_qr_equivalence(data_vandermonde):
+    x = data_vandermonde
+
+    qr_sensors = QR().get_sensors(x.T)
+
+    costs = np.zeros(x.shape[1])
+    ccqr_senors = CCQR(sensor_costs=costs).get_sensors(x.T)
+
+    np.testing.assert_array_equal(qr_sensors, ccqr_senors)

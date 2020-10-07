451a611806cdaab08c2ce11c8710da5d33e180d9
briandesilva <bdesilva@uw.edu>
2020-10-07 09:42:40 -0700

    CCQR test

--- test/optimizers/test_optimizers.py
+++ test/optimizers/test_optimizers.py
@@ -20,6 +20,20 @@ def test_ccqr_qr_equivalence(data_vandermonde):
     qr_sensors = QR().get_sensors(x.T)
 
     costs = np.zeros(x.shape[1])
-    ccqr_senors = CCQR(sensor_costs=costs).get_sensors(x.T)
+    ccqr_sensors = CCQR(sensor_costs=costs).get_sensors(x.T)
 
-    np.testing.assert_array_equal(qr_sensors, ccqr_senors)
+    np.testing.assert_array_equal(qr_sensors, ccqr_sensors)
+
+
+def test_ccqr_sensor_placement(data_random):
+    x = data_random
+
+    forbidden_sensors = np.arange(0, x.shape[1], 3)
+    costs = np.zeros(x.shape[1])
+    costs[forbidden_sensors] = 100
+    # Get ranked sensors
+    sensors = CCQR(sensor_costs=costs).get_sensors(x.T)
+
+    # Forbidden sensors should not be included
+    chosen_sensors = set(sensors[: (x.shape[1] - len(forbidden_sensors))])
+    assert chosen_sensors.isdisjoint(set(forbidden_sensors))

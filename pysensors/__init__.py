aef436223c1830c51ca524f9b20f900719442dac
briandesilva <bdesilva@uw.edu>
2020-10-13 11:14:53 -0700

    Fix inference of version

--- pysensors/__init__.py
+++ pysensors/__init__.py
@@ -2,7 +2,7 @@ from pkg_resources import DistributionNotFound
 from pkg_resources import get_distribution
 
 try:
-    __version__ = get_distribution(__name__).version
+    __version__ = get_distribution("python-sensors").version
 except DistributionNotFound:
     pass
 

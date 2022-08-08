885add494ba3602691979142f5f08c222bb860cf
niharika2999 <niharika.karnik29@@gmail.com>
2022-08-08 15:47:56 -0600

    Adding _validation in utils with determinant and relative reconstruction error

--- pysensors/utils/__init__.py
+++ pysensors/utils/__init__.py
@@ -5,6 +5,8 @@ from ._constraints import get_constraind_sensors_indices
 from ._constraints import get_constrained_sensors_indices_linear
 from ._constraints import box_constraints
 from ._constraints import functional_constraints
+from ._validation import determinant
+from ._validation import relative_reconstruction_error
 
 __all__ = [
     "constrained_binary_solve",
@@ -13,5 +15,7 @@ __all__ = [
     "get_constraind_sensors_indices",
     "get_constrained_sensors_indices_linear",
     "box_constraints",
-    "functional_constraints"
+    "functional_constraints",
+    "determinant",
+    "relative_reconstruction_error"
 ]

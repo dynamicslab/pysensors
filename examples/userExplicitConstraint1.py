dc1c099d52c674f148ea3244f4aa930c201dcb7f
Jimmy-INL <mohammad.abdo@inl.gov>
2022-12-29 14:42:49 -0700

    adding the user-defined constraints

---
+++ examples/userExplicitConstraint1.py
@@ -0,0 +1,4 @@
+
+def userExplicitConstraint1(x,y,**kwargs):
+  g = (x-30)**2 + (y-40)**2 - 100
+  return g
---
+++ examples/userExplicitConstraint2.py
@@ -0,0 +1,4 @@
+
+def userExplicitConstraint2(x,y,**kwargs):
+  g = (x-20)**2 + (y-10)**2 - 49
+  return g

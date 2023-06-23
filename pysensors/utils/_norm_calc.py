69f11239e4e1c0c90d9873641b46422fcf1e0222
Jimmy-INL <mohammad.abdo@inl.gov>
2023-06-23 10:47:15 -0600

    removing a for loop

--- pysensors/utils/_norm_calc.py
+++ pysensors/utils/_norm_calc.py
@@ -39,13 +39,13 @@ def exact_n(lin_idx, dlens, piv, j, n_const_sensors, **kwargs): ##Will first for
     else:
         n_sensors = len(all_sensors)
     count = np.count_nonzero(np.isin(all_sensors[:n_sensors],lin_idx,invert=False))
-    for i in range(n_sensors):
-        if np.isin(all_sensors[:n_sensors],lin_idx,invert=False).sum() < n_const_sensors:
-            if n_sensors > j >= (n_sensors - (n_const_sensors - count)):
-                didx = np.isin(piv[j:],lin_idx,invert=True)
-                dlens[didx] = 0
-        else:
-            dlens = max_n(lin_idx, dlens, piv, j, n_const_sensors, **kwargs)
+    # for i in range(n_sensors):
+    if np.isin(all_sensors[:n_sensors],lin_idx,invert=False).sum() < n_const_sensors:
+        if n_sensors > j >= (n_sensors - (n_const_sensors - count)):
+            didx = np.isin(piv[j:],lin_idx,invert=True)
+            dlens[didx] = 0
+    else:
+        dlens = max_n(lin_idx, dlens, piv, j, n_const_sensors, **kwargs)
     return(dlens)
 
 def max_n(lin_idx, dlens, piv, j, n_const_sensors, **kwargs):
